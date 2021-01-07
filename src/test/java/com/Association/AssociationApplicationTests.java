05af8b4a352f12350b9200f659e5283a1a1566e1
mustapha <m.touil.mt@gmail.com>
2021-01-07 12:57:44 +0100

    Initial commit

---
+++ src/test/java/com/Association/AssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
