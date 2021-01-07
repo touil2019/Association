3641bd85710baceb93497dad46ac62eea5ad1008
mustapha <m.touil.mt@gmail.com>
2021-01-07 13:58:02 +0100

    Creation des packages et du dossier templates. Ajout des dependances Spring Boot JDBC,JPA,Security,postgresql,log4j2,Thymleaf.

--- src/main/java/com/Association/AssociationApplication.java
+++ src/main/java/com/Association/AssociationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssociationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AssociationApplication.class, args);
 	}
 
